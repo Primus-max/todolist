d29fcb2d1843105adc194a66e90110ce33afc33d
Vladimir <urkytsk3@gmail.com>
2022-01-16 21:32:13 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<TabMenu :model="items" />
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+	data() {
+		return {
+			items: [
+                {label: 'Home', icon: 'pi pi-fw pi-home', to:'/'},                
+                {label: 'About', icon: 'pi pi-fw pi-prime', to:'/about'},                              
+            ]
+		}
+	}
 }
 </script>
+
 
 <style>
 #app {
@@ -21,6 +28,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,18 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import 'primevue/resources/themes/bootstrap4-light-purple/theme.css'; //theme
+import 'primevue/resources/primevue.min.css'; //core css
+import 'primeicons/primeicons.css'; //icons
 
-createApp(App).mount('#app')
+import { createApp } from 'vue';
+
+import PrimeVue from 'primevue/config';
+import TabMenu from 'primevue/tabmenu';
+
+import App from './App.vue';
+import router from './router';
+
+const app = createApp(App);
+
+app.component('TabMenu', TabMenu)
+app.use(router)
+app.use(PrimeVue)
+app.mount('#app')
